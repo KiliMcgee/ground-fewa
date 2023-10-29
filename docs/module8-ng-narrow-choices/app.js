e1bbe154c09aa71fed1daa70f0333acc0d5cf2d1
Logan Harris <lharris@atlassian.com>
2023-10-29 17:55:07 -0400

    Fix for custom directive. Functional menu filter/narrow application

--- docs/module8-ng-narrow-choices/app.js
+++ docs/module8-ng-narrow-choices/app.js
@@ -2,34 +2,37 @@
     'use strict';
 
     angular.module('NarrowChoicesApp', [])
+        .constant('ApiBasePath', "https://coursera-jhu-default-rtdb.firebaseio.com")
         .controller('NarrowChoicesController', NarrowChoicesController)
         .service('MenuSearchService', MenuSearchService)
-        .constant('ApiBasePath', "https://coursera-jhu-default-rtdb.firebaseio.com")
         .directive('foundItems', FoundItemsDirective);
 
     // NarrowChoicesController logic below
     NarrowChoicesController.$inject = ['MenuSearchService'];
     function NarrowChoicesController(MenuSearchService) {
         var narrowCtlr = this;
-
+        narrowCtlr.clicked = false;
         narrowCtlr.searchTerm = undefined;
-        narrowCtlr.foundItems = [];
-        // TODO - Figure out how to delay the error until button clicked? Cookie example.
-        narrowCtlr.isInvalidSearch = () => !narrowCtlr.foundItems?.length || !narrowCtlr.searchTerm
+        narrowCtlr.foundItemsList = undefined;
 
-        // Exposing the buy method on the controller that utilizes a method from the service
+        // Exposing the search method on the controller that utilizes a MenuSearchService method
         narrowCtlr.search = function () {
+            if (narrowCtlr.searchTerm === "" || !narrowCtlr.searchTerm) {
+                // If the search term is empty, return an empty list
+                narrowCtlr.foundItemsList = [];
+                return;
+            }
             MenuSearchService.getMatchedMenuItems(narrowCtlr.searchTerm)
             .then((response) => {
                 console.log('Found items response from search service: ', response);
-                narrowCtlr.foundItems = response;
+                narrowCtlr.foundItemsList = response;
             })
             .catch((error) => console.log('Caught error from MenuSearchService.getMatchedMenuItems: ', error));
         }
 
         narrowCtlr.removeItemAtIndex = function (index) {
             console.log('Removing item at index: ', index);
-            narrowCtlr.foundItems.splice(index, 1);
+            narrowCtlr.foundItemsList.splice(index, 1);
         }
     }
 
@@ -37,7 +40,6 @@
     MenuSearchService.$inject = ['$http', '$q', 'ApiBasePath'];
     function MenuSearchService($http, $q, ApiBasePath) {
         var service = this;
-
         var itemsResponse = [];
 
         service.getMatchedMenuItems = function (searchTerm) {
@@ -69,11 +71,9 @@
         var ddo = {
             templateUrl: 'foundItems.html',
             scope: {
-                foundItems: '<',
-                onRemove: '&'
-            },
-            controller: 'NarrowChoicesController as narrowCtlr',
-            bindToController: true
+                'items': '<',
+                'onRemove': '&'
+            }
         };
 
         return ddo;
